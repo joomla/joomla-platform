f95d50b9fcfd84f42e00dbe87a9e9631276f955b
Rouven Weßling <me@rouvenwessling.de>
2011-07-09 14:52:49 +0200

    Sync more changes from Joomla 1.7. (Mostly the media folder)

--- libraries/joomla/form/rules/options.php
+++ libraries/joomla/form/rules/options.php
@@ -2,8 +2,9 @@
 /**
  * @package     Joomla.Platform
  * @subpackage  Form
- * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ *
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
--- media/system/js/multiselect.js
+++ media/system/js/multiselect.js
@@ -1,5 +1,5 @@
 /**
- * @version		$Id: multiselect.js 20196 2011-01-09 02:40:25Z ian $
+ * @version		$Id: multiselect.js 21554 2011-06-17 14:36:22Z chdemko $
  * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
@@ -18,10 +18,13 @@
 		},
 		doselect: function(e) {
 			var current = document.id(e.target);
-			if(e.shift && $type(this.last) !== false){
-				var checked = current.getProperty('checked')  ? 'checked' : '';
-				var range = [this.boxes.indexOf(current), this.boxes.indexOf(this.last)].sort();
-				for(var i=range[0]; i <= range[1]; i++){
+			if (e.shift && typeOf(this.last) !== 'null') {
+				var checked = current.getProperty('checked') ? 'checked' : '';
+				var range = [this.boxes.indexOf(current), this.boxes.indexOf(this.last)].sort(function(a, b) {
+					//Shorthand to make sort() sort numerical instead of lexicographic
+					return a-b;
+				});
+				for (var i=range[0]; i <= range[1]; i++) {
 					this.boxes[i].setProperty('checked', checked);
 				}
 			}
