70404b4d317e2f5815f56b49becabf7b23364ee9
Michael Babker <mbabker@flbab.com>
2011-10-18 13:14:20 +0430

    Make the default unit null

--- libraries/joomla/html/html/date.php
+++ libraries/joomla/html/html/date.php
@@ -30,7 +30,7 @@ abstract class JHtmlDate
 	 *
 	 * @since   11.3
 	 */
-	public static function relative($date, $unit = null, $time = null)
+	public static function relative($date, $unit = null)
 	{
 		if (is_null($time))
 		{
