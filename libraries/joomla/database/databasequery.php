0099d35f6ebb52ff53e6a56d5abd6613ff2d1087
elinw <elin.waring@gmail.com>
2011-11-10 22:18:11 -0500

    Add missing __clone magic method to JDatabaseQuery and JDatabaseQueryElement.

--- libraries/joomla/database/databasequery.php
+++ libraries/joomla/database/databasequery.php
@@ -1194,8 +1194,7 @@ abstract class JDatabaseQuery
 	}
 
 	/**
-	 * Method to provide deep copy support to nested objects and
-	 * arrays when cloning.
+	 * Method to lock the database table for writing.
 	 *
 	 * @return  void
 	 *
