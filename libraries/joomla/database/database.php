a87697357ad2ae81b6f7fb7ec192fcf1e12c927f
Gabriele Pongelli <gabriele.pongelli@gmail.com>
2011-11-20 16:44:54 +0100

    Added a check of static $cursor in loadNexObject and loadNextRow. Without this check the row or object returned is always the first of result set.

--- libraries/joomla/database/database.php
+++ libraries/joomla/database/database.php
@@ -977,9 +977,12 @@ abstract class JDatabase implements JDatabaseInterface
 		static $cursor;
 
 		// Execute the query and get the result set cursor.
-		if (!($cursor = $this->query()))
+		if ( is_null($cursor) )
 		{
-			return $this->errorNum ? null : false;
+			if (!($cursor = $this->query()))
+			{
+				return $this->errorNum ? null : false;
+			}
 		}
 
 		// Get the next row from the result set as an object of type $class.
@@ -1008,9 +1011,12 @@ abstract class JDatabase implements JDatabaseInterface
 		static $cursor;
 
 		// Execute the query and get the result set cursor.
-		if (!($cursor = $this->query()))
+		if ( is_null($cursor) )
 		{
-			return $this->errorNum ? null : false;
+			if (!($cursor = $this->query()))
+			{
+				return $this->errorNum ? null : false;
+			}
 		}
 
 		// Get the next row from the result set as an object of type $class.
