ed222d1c05c3fc8e45e8854e9f3c1064214c8543
Gabriele Pongelli <gabriele.pongelli@gmail.com>
2011-11-20 14:14:16 +0100

    Code style cleaning.

--- tests/includes/JoomlaPostgreSQLTestCase.php
+++ tests/includes/JoomlaPostgreSQLTestCase.php
@@ -1,18 +1,21 @@
 <?php
 /**
  * @package    Joomla.UnitTest
- *
- * @copyright  Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE
+ * @author     gpongelli <gabriele.pongelli@gmail.com>
+ * 
+ * @copyright  Copyright (C) 2005 - 2011 Open Source Matters. All rights reserved.
+ * @license    GNU General Public License
  */
 
-require_once dirname(__FILE__).'/JoomlaTestCase.php';
+require_once dirname(__FILE__) . '/JoomlaTestCase.php';
 
 /**
  * Test case class for Joomla Unit Testing
  *
- * @package  Joomla.UnitTest
- * @since    11.1
+ * @package     Joomla.UnitTest
+ * @subpackage  Database
+ * 
+ * @since       11.3
  */
 class JoomlaPostgreSQLTestCase extends JoomlaTestCase
 {
@@ -26,8 +29,8 @@ class JoomlaPostgreSQLTestCase extends JoomlaTestCase
 	protected function getMockDatabase()
 	{
 		// Load the real class first otherwise the mock will be used if jimport is called again.
-		require_once JPATH_PLATFORM.'/joomla/database/database.php';
-		
+		require_once JPATH_PLATFORM . '/joomla/database/database.php';
+
 		// Load the mock class builder.
 		require_once JPATH_TESTS . '/includes/mocks/JDatabasePostgreSQLMock.php';
 
