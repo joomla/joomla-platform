47e7b3916f31582e7481b3bdc10513d1f56663de
Gabriele Pongelli <gabriele.pongelli@gmail.com>
2011-11-20 14:13:42 +0100

    Code style cleaning.

--- tests/includes/mocks/JDatabasePostgreSQLQueryDummy.php
+++ tests/includes/mocks/JDatabasePostgreSQLQueryDummy.php
@@ -1,17 +1,21 @@
 <?php
 /**
  * @package    Joomla.UnitTest
+ * @author     gpongelli <gabriele.pongelli@gmail.com>
+ * 
  * @copyright  Copyright (C) 2005 - 2011 Open Source Matters. All rights reserved.
  * @license    GNU General Public License
  */
 
-require_once JPATH_PLATFORM.'/joomla/database/database/postgresqlquery.php';
+require_once JPATH_PLATFORM . '/joomla/database/database/postgresqlquery.php';
 
 /**
  * Dummy type of JDatabaseQueryPostgreSQL class for testing.
  *
- * @package  Joomla.UnitTest
- * @since    11.3
+ * @package     Joomla.UnitTest
+ * @subpackage  Database
+ * 
+ * @since       11.3
  */
 class JDatabasePostgreSQLQueryDummy extends JDatabaseQueryPostgreSQL
 {
