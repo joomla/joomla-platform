61a9ef7fc668a1b116b91efbc62c0761e56166fe
Gabriele Pongelli <gabriele.pongelli@gmail.com>
2011-11-29 07:36:45 +0100

    Removed unneeded require_once of databasequery.php, already loaded inside postgresqlquery.php .

---
+++ tests/suite/joomla/database/JDatabasePostgreSQLQueryInspector.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @version    $Id: JDatabasePostgreSQLQueryInspector.php gpongelli $
+ * @package    Joomla.UnitTest
+ * 
+ * @copyright  Copyright (C) 2005 - 2011 Open Source Matters. All rights reserved.
+ * @license    GNU General Public License
+ */
+
+require_once JPATH_PLATFORM . '/joomla/database/database/postgresqlquery.php';
+
+/**
+ * Class to expose protected properties and methods in JDatabaseQueryExporter for testing purposes.
+ *
+ * @package     Joomla.UnitTest
+ * @subpackage  Database
+ * 
+ * @since       11.3
+ */
+class JDatabasePostgreSQLQueryInspector extends JDatabaseQueryPostgreSQL
+{
+	/**
+	 * Sets any property from the class.
+	 *
+	 * @param   string  $property  The name of the class property.
+	 * @param   string  $value     The value of the class property.
+	 *
+	 * @return  void
+	 */
+	public function __set($property, $value)
+	{
+		return $this->$property = $value;
+	}
+
+	/**
+	 * Gets any property from the class.
+	 *
+	 * @param   string  $property  The name of the class property.
+	 *
+	 * @return  mixed   The value of the class property.
+	 */
+	public function get($property)
+	{
+		return $this->$property;
+	}
+}
