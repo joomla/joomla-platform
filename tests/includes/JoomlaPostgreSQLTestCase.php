48cfa647329030dfd0ed28ae98306b454c106824
Gabriele Pongelli <gabriele.pongelli@gmail.com>
2011-12-02 00:58:06 +0100

    Added files lost in last rebase with master branch.

---
+++ tests/includes/JoomlaPostgreSQLTestCase.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @version    $Id: JDatabasePostgreSQLTestCase.php gpongelli $
+ * @package    Joomla.UnitTest
+ * 
+ * @copyright  Copyright (C) 2005 - 2011 Open Source Matters. All rights reserved.
+ * @license    GNU General Public License
+ */
+
+require_once dirname(__FILE__) . '/JoomlaTestCase.php';
+
+/**
+ * Test case class for Joomla Unit Testing
+ *
+ * @package     Joomla.UnitTest
+ * @subpackage  Database
+ * 
+ * @since       11.3
+ */
+class JoomlaPostgreSQLTestCase extends JoomlaTestCase
+{
+	/**
+	 * Gets a mock database object.
+	 *
+	 * @return  object
+	 *
+	 * @since   11.3
+	 */
+	protected function getMockDatabase()
+	{
+		// Load the real class first otherwise the mock will be used if jimport is called again.
+		require_once JPATH_PLATFORM . '/joomla/database/database.php';
+
+		// Load the mock class builder.
+		require_once JPATH_TESTS . '/includes/mocks/JDatabasePostgreSQLMock.php';
+
+		return JDatabasePostgreSQLMock::create($this);
+	}
+}
