64661f1a11894a77d5cf482282b46547c9c0827b
eddieajau <andrew.eddie@newlifeinit.com>
2012-02-20 21:07:30 +1000

    Add DB legacy support back in.

---
+++ libraries/joomla/database/database.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Database
+ *
+ * @copyright   Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+// Deprecation warning.
+JLog::add('JDatabase is deprecated, use JDatabaseDriver instead.', JLog::NOTICE, 'deprecated');
+
+/**
+ * Database connector class.
+ *
+ * @package     Joomla.Platform
+ * @subpackage  Database
+ * @since       11.1
+ * @deprecated  13.1
+ */
+abstract class JDatabase extends JDatabaseDriver
+{
+	/**
+	 * Execute the SQL statement.
+	 *
+	 * @return  mixed  A database cursor resource on success, boolean false on failure.
+	 *
+	 * @since   11.1
+	 * @throws  RuntimeException
+	 */
+	public function query()
+	{
+		return $this->execute();
+	}
+
+	/**
+	 * Gets the error message from the database connection.
+	 *
+	 * @param   boolean  $escaped  True to escape the message string for use in JavaScript.
+	 *
+	 * @return  string  The error message for the most recent query.
+	 *
+	 * @deprecated  12.1
+	 * @since   11.1
+	 */
+	public function getErrorMsg($escaped = false)
+	{
+		// Deprecation warning.
+		JLog::add('JDatabaseDriver::getErrorMsg() is deprecated, use exception handling instead.', JLog::WARNING, 'deprecated');
+
+		if ($escaped)
+		{
+			return addslashes($this->errorMsg);
+		}
+		else
+		{
+			return $this->errorMsg;
+		}
+	}
+
+	/**
+	 * Gets the error number from the database connection.
+	 *
+	 * @return      integer  The error number for the most recent query.
+	 *
+	 * @since       11.1
+	 * @deprecated  12.1
+	 */
+	public function getErrorNum()
+	{
+		// Deprecation warning.
+		JLog::add('JDatabaseDriver::getErrorNum() is deprecated, use exception handling instead.', JLog::WARNING, 'deprecated');
+
+		return $this->errorNum;
+	}
+
+	/**
+	 * Return the most recent error message for the database connector.
+	 *
+	 * @param   boolean  $showSQL  True to display the SQL statement sent to the database as well as the error.
+	 *
+	 * @return  string  The error message for the most recent query.
+	 *
+	 * @deprecated  12.1
+	 * @since   11.1
+	 */
+	public function stderr($showSQL = false)
+	{
+		// Deprecation warning.
+		JLog::add('JDatabaseDriver::stderr() is deprecated.', JLog::WARNING, 'deprecated');
+
+		if ($this->errorNum != 0)
+		{
+			return JText::sprintf('JLIB_DATABASE_ERROR_FUNCTION_FAILED', $this->errorNum, $this->errorMsg)
+			. ($showSQL ? "<br />SQL = <pre>$this->sql</pre>" : '');
+		}
+		else
+		{
+			return JText::_('JLIB_DATABASE_FUNCTION_NOERROR');
+		}
+	}
+}
