3c2e1370a2dde690ecbee488fdb29a8221d1ea07
romacron <info@romacron.de>
2012-03-06 12:31:36 +0100

    the db instancer called to late for sterr so replaced the JFactory::getDBO() up on the method var creators

--- libraries/joomla/installer/adapters/file.php
+++ libraries/joomla/installer/adapters/file.php
@@ -390,6 +390,7 @@ class JInstallerFile extends JAdapterInstance
 	public function uninstall($id)
 	{
 		// Initialise variables.
+		$db = JFactory::getDbo();
 		$row = JTable::getInstance('extension');
 		if (!$row->load($id))
 		{
@@ -489,8 +490,7 @@ class JInstallerFile extends JAdapterInstance
 			}
 
 			// Remove the schema version
-			$db = JFactory::getDbo();
-			$query = $db->getQuery(true);
+		        $query = $db->getQuery(true);
 			$query->delete()
 				->from('#__schemas')
 				->where('extension_id = ' . $row->extension_id);
