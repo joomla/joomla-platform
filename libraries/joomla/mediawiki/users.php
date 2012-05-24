567b2624ed95188fcdee6b23e2af9d45ce446631
Prasath Nadarajah <n.prasath.002@gmail.com>
2012-05-24 08:28:47 +0530

    Appended data with URL

--- libraries/joomla/mediawiki/users.php
+++ libraries/joomla/mediawiki/users.php
@@ -32,18 +32,10 @@ class JMediawikiUsers extends JMediawikiObject
     public function login($username, $password, $token = '')
     {
         // Build the request path.
-        $path = '?action=login';
-
-        // Build the request data.
-        $data = json_encode(
-            array(
-                'lgname' => $username,
-                'lgpassword' => $password
-            )
-        );
+        $path = '?action=login&lgname=' . $username . '&lgpassword=' . $password;
 
         // Send the request.
-        $response = $this->client->post($this->fetchUrl($path), $data);
+        $response = $this->client->post($this->fetchUrl($path));
 
         return $response;
     }
