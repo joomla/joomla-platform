d7b83dc74d47ca6a715ba78c6fed4184896411ee
Prasath Nadarajah <n.prasath.002@gmail.com>
2012-05-29 09:29:03 +0530

    Implemented getCategoriesInfo

--- libraries/joomla/mediawiki/categories.php
+++ libraries/joomla/mediawiki/categories.php
@@ -47,13 +47,24 @@ class JMediawikiCategories extends JMediawikiObject
     /**
      * Method to get information about all categories used.
      *
+     * @param   array       $titles             Page titles to retrieve links.
+     *
      * @return  object
      *
      * @since   12.1
      */
-    public function getAllCategories()
+    public function getCategoriesInfo(array $titles)
     {
+        // build the request
+        $path = '?action=query&generator=categories&prop=info';
 
+        // append titles to the request
+        $path .= '&titles=' . $this->buildParameter($titles);
+
+        // Send the request.
+        $response = $this->client->get($this->fetchUrl($path));
+
+        return $this->validateResponse($response);
     }
 
     /**
