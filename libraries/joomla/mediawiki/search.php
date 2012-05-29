70529bd7ff17e57add55e75f45bf37a38b65c4e6
Prasath Nadarajah <n.prasath.002@gmail.com>
2012-05-29 12:09:51 +0530

    Added docs for search

--- libraries/joomla/mediawiki/search.php
+++ libraries/joomla/mediawiki/search.php
@@ -22,11 +22,20 @@ class JMediawikiSearch extends JMediawikiObject
     /**
      * Method to perform a full text search.
      *
+     * @param   string      $srsearch           Search for all page titles (or content) that has this value.
+     * @param   array       $srnamespace        The namespace(s) to enumerate.
+     * @param   string      $srwhat             Search inside the text or titles.
+     * @param   array       $srinfo             What metadata to return.
+     * @param   array       $srprop             What properties to return.
+     * @param   boolean     $srredirects        Include redirect pages in the search.
+     * @param   integer     $sroffest           Use this value to continue paging.
+     * @param   integer     $srlimit            How many total pages to return.
+     *
      * @return  object
      *
      * @since   12.1
      */
-    public function search()
+    public function search($srsearch, array $srnamespace = null, $srwhat = null, array $srinfo = null, array $srprop = null, $srredirects = null, $sroffest = null, $srlimit = null)
     {
 
     }
