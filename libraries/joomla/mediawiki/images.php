9e88e192a993ee6d63ad81a682ab663cd37f2d1a
Prasath Nadarajah <n.prasath.002@gmail.com>
2012-05-29 13:39:34 +0530

    getImageInfo implemented

--- libraries/joomla/mediawiki/images.php
+++ libraries/joomla/mediawiki/images.php
@@ -88,13 +88,65 @@ class JMediawikiImages extends JMediawikiObject
     /**
      * Method to get all image information and upload history.
      *
+     * @param   array       $liprop                     What image information to get.
+     * @param   integer     $lilimit                    How many image revisions to return.
+     * @param   string      $listart                    Timestamp to start listing from.
+     * @param   string      $liend                      Timestamp to stop listing at.
+     * @param   integer     $liurlwidth                 URL to an image scaled to this width will be returned..
+     * @param   integer     $liurlheight                URL to an image scaled to this height will be returned.
+     * @param   string      $limetadataversion          Version of metadata to use.
+     * @param   string      $liurlparam                 A handler specific parameter string.
+     * @param   boolean     $licontinue                 When more results are available, use this to continue.
+     *
      * @return  object
      *
      * @since   12.1
      */
-    public function getImageInfo()
+    public function getImageInfo(array $liprop = null, $lilimit = null, $listart = null, $liend = null, $liurlwidth = null, $liurlheight = null, $limetadataversion = null, $liurlparam = null, $licontinue = null)
     {
+        // build the request
+        $path = '?action=query&meta=siteinfo';
 
+        if (isset($liprop)) {
+            $path .= '&liprop=' . $this->buildParameter($liprop);
+        }
+
+        if (isset($lilimit)) {
+            $path .= '&lilimit=' . $lilimit;
+        }
+
+        if (isset($listart)) {
+            $path .= '&listart=' . $listart;
+        }
+
+        if (isset($liend)) {
+            $path .= '&liend=' . $liend;
+        }
+
+        if (isset($liurlwidth)) {
+            $path .= '&liurlwidth=' . $liurlwidth;
+        }
+
+        if (isset($liurlheight)) {
+            $path .= '&liurlheight=' . $liurlheight;
+        }
+
+        if (isset($limetadataversion)) {
+            $path .= '&limetadataversion=' . $limetadataversion;
+        }
+
+        if (isset($liurlparam)) {
+            $path .= '&liurlparam=' . $liurlparam;
+        }
+
+        if ($licontinue) {
+            $path .= '&alcontinue=';
+        }
+
+        // Send the request.
+        $response = $this->client->get($this->fetchUrl($path));
+
+        return $this->validateResponse($response);
     }
 
     /**
