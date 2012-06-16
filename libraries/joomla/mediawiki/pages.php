cbd158b9cef3ca292a19432fd31af49934d547ce
Prasath Nadarajah <n.prasath.002@gmail.com>
2012-06-16 11:46:07 +0530

    test commit

--- libraries/joomla/mediawiki/pages.php
+++ libraries/joomla/mediawiki/pages.php
@@ -294,7 +294,7 @@ class JMediawikiPages extends JMediawikiObject
      *
      * @since   12.1
      */
-    public function getIWBackLinks()
+    public function getIWBackLinks($bltitle)
     {
 
     }
