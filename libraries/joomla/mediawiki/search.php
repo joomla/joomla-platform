e098cb9d06349fc9d538738277636d8594abc433
Prasath Nadarajah <n.prasath.002@gmail.com>
2012-06-18 14:31:24 -0700

    codesniff cleanup

--- libraries/joomla/mediawiki/search.php
+++ libraries/joomla/mediawiki/search.php
@@ -19,7 +19,7 @@ defined('JPATH_PLATFORM') or die;
 class JMediawikiSearch extends JMediawikiObject
 {
 
-    /**
+	/**
      * Method to perform a full text search.
      *
      * @param   string      $srsearch           Search for all page titles (or content) that has this value.
@@ -35,90 +35,103 @@ class JMediawikiSearch extends JMediawikiObject
      *
      * @since   12.1
      */
-    public function search($srsearch, array $srnamespace = null, $srwhat = null, array $srinfo = null, array $srprop = null, $srredirects = null, $sroffest = null, $srlimit = null)
-    {
-        // Build the request.
-        $path = '?action=query&list=search';
+	public function search($srsearch, array $srnamespace = null, $srwhat = null, array $srinfo = null, array $srprop = null, $srredirects = null, $sroffest = null, $srlimit = null)
+	{
+		// Build the request.
+		$path = '?action=query&list=search';
 
-        if (isset($srsearch)) {
-            $path .= '&srsearch=' . $srsearch;
-        }
+		if (isset($srsearch))
+		{
+			$path .= '&srsearch=' . $srsearch;
+		}
 
-        if (isset($srnamespace)) {
-            $path .= '&srnamespace=' . $this->buildParameter($srnamespace);
-        }
+		if (isset($srnamespace))
+		{
+			$path .= '&srnamespace=' . $this->buildParameter($srnamespace);
+		}
 
-        if (isset($srwhat)) {
-            $path .= '&srwhat=' . $srwhat;
-        }
+		if (isset($srwhat))
+		{
+			$path .= '&srwhat=' . $srwhat;
+		}
 
-        if (isset($srinfo)) {
-            $path .= '&srinfo=' . $this->buildParameter($srinfo);
-        }
+		if (isset($srinfo))
+		{
+			$path .= '&srinfo=' . $this->buildParameter($srinfo);
+		}
 
-        if (isset($srprop)) {
-            $path .= '&srprop=' . $this->buildParameter($srprop);
-        }
+		if (isset($srprop))
+		{
+			$path .= '&srprop=' . $this->buildParameter($srprop);
+		}
 
-        if ($srredirects) {
-            $path .= '&srredirects=';
-        }
+		if ($srredirects)
+		{
+			$path .= '&srredirects=';
+		}
 
-        if (isset($sroffest)) {
-            $path .= '&sroffest=' . $sroffest;
-        }
+		if (isset($sroffest))
+		{
+			$path .= '&sroffest=' . $sroffest;
+		}
 
-        if (isset($srlimit)) {
-            $path .= '&srlimit=' . $srlimit;
-        }
+		if (isset($srlimit))
+		{
+			$path .= '&srlimit=' . $srlimit;
+		}
 
-        // Send the request.
-        $response = $this->client->get($this->fetchUrl($path));
+		// Send the request.
+		$response = $this->client->get($this->fetchUrl($path));
 
-        return $this->validateResponse($response);
-    }
+		return $this->validateResponse($response);
+	}
 
-    /**
+	/**
      * Method to search the wiki using opensearch protocol.
      *
      * @param   string      $search             Search string.
-     * @param   integer     $limit              Maximum amount of results to return.
-     * @param   array       $namespace          Namespaces to search.
-     * @param   string      $suggest            Do nothing if $wgEnableOpenSearchSuggest is false.
+	 * @param	integer		$limit				Maximum amount of results to return.
+     * @param   array       $namespace			Namespaces to search.
+     * @param   string      $suggest			Do nothing if $wgEnableOpenSearchSuggest is false.
      * @param   string      $format             Output format.
      *
      * @return  object
      *
      * @since   12.1
      */
-    public function openSearch($search = null, $limit = null, array $namespace = null, $suggest = null, $format = null)
-    {
-        // build the request
-        $path = '?action=query&list=search';
+	public function openSearch($search = null, $limit = null, array $namespace = null, $suggest = null, $format = null)
+	{
+		// Build the request.
+		$path = '?action=query&list=search';
 
-        if (isset($search)) {
-            $path .= '&search=' . $search;
-        }
+		if (isset($search))
+		{
+			$path .= '&search=' . $search;
+		}
 
-        if (isset($limit)) {
-            $path .= '&limit=' . $limit;
-        }
+		if (isset($limit))
+		{
+			$path .= '&limit=' . $limit;
+		}
 
-        if (isset($namespace)) {
-            $path .= '&namespace=' . $this->buildParameter($namespace);
-        }
+		if (isset($namespace))
+		{
+			$path .= '&namespace=' . $this->buildParameter($namespace);
+		}
 
-        if (isset($suggest)) {
-            $path .= '&suggest=' . $suggest;
-        }
+		if (isset($suggest))
+		{
+			$path .= '&suggest=' . $suggest;
+		}
 
-        if (isset($format)) {
-            $path .= '&format=' . $format;
-        }
+		if (isset($format))
+		{
+			$path .= '&format=' . $format;
+		}
 
-        // Send the request.
-        $response = $this->client->get($this->fetchUrl($path));
+		// Send the request.
+		$response = $this->client->get($this->fetchUrl($path));
 
-        return $this->validateResponse($response);
-    }
+		return $this->validateResponse($response);
+	}
 }
