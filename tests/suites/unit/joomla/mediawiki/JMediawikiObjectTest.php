125b2660bb54f722bca307752a93ee73349a6b51
Prasath Nadarajah <n.prasath.002@gmail.com>
2012-07-14 22:52:06 +0530

    unit tests

--- tests/suites/unit/joomla/mediawiki/JMediawikiObjectTest.php
+++ tests/suites/unit/joomla/mediawiki/JMediawikiObjectTest.php
@@ -9,56 +9,72 @@
 
 require_once JPATH_PLATFORM . '/joomla/mediawiki/http.php';
 require_once JPATH_PLATFORM . '/joomla/mediawiki/object.php';
-require_once __DIR__ . '/stubs/JGithubObjectMock.php';
+require_once __DIR__ . '/stubs/JMediawikiObjectMock.php';
 
 /**
  * Test class for JGithub.
  */
 class JMediawikiObjectTest extends PHPUnit_Framework_TestCase
 {
-    /**
-     * @var JMediawikiObject
-     */
-    protected $object;
+	/**
+	 * @var    JRegistry  Options for the Mediawiki object.
+	 * @since  12.1
+	 */
+	protected $options;
 
-    /**
+	/**
+	 * @var    JGithubHttp  Mock client object.
+	 * @since  12.1
+	 */
+	protected $client;
+
+	/**
+	 * @var    JMediawikiObject  Object under test.
+	 * @since  12.1
+	 */
+	protected $object;
+
+	/**
      * Sets up the fixture, for example, opens a network connection.
      * This method is called before a test is executed.
      */
-    protected function setUp()
-    {
-        $this->object = new JMediawikiObject;
-    }
+	protected function setUp()
+	{
+		$this->options = new JRegistry;
+		$this->client = $this->getMock('JMediawikiHttp', array('get', 'post'));
 
-    /**
+		$this->object = new JMediawikiObjectMock($this->options, $this->client);
+	}
+
+	/**
      * Tears down the fixture, for example, closes a network connection.
      * This method is called after a test is executed.
      */
-    protected function tearDown()
-    {
-    }
+	protected function tearDown()
+	{
+	}
 
-    /**
+	/**
      * @covers JMediawikiObject::buildParameter
      * @todo   Implement testBuildParameter().
      */
-    public function testBuildParameter()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
+	public function testBuildParameter()
+	{
+		$this->assertThat(
+			$this->object->buildParameter(array('mango', 'apple', 'orange')),
+			$this->equalTo('mango|apple|orange')
+		);
+	}
 
-    /**
+	/**
      * @covers JMediawikiObject::validateResponse
      * @todo   Implement testValidateResponse().
-     */
-    public function testValidateResponse()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-    }
+    */
+	public function testValidateResponse()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 }
