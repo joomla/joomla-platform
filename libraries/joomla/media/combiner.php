f203de12a4d5273eb98892a1902193c20fc1dbe3
kaviththiranga <rc404mekt@gmail.com>
2012-07-18 17:21:57 +0530

    fixes

--- libraries/joomla/media/combiner.php
+++ libraries/joomla/media/combiner.php
@@ -14,23 +14,29 @@ defined('JPATH_PLATFORM') or die;
  *
  * @package     Joomla.Platform
  * @subpackage  Media
- * @since  12.1 
+ * @since       12.1 
  */
-abstract class JMediaCombiner 
+abstract class JMediaCombiner
 {
 	public $sources = array();
-	
-	protected $_combined = null;
-	
-	protected $_options = array();
-	
 
+	protected $_combined = null;
+
+	protected $_options = array();
+
+	/**
+	 * Constructor
+	 * 
+	 * @param   Array  $options  options for the combiner
+	 * 
+	 * @since   12.1 
+	 */
 	public function __construct($options = array())
 	{
 		// Merge user defined options with default options
 		$this->_options = array_merge($options, $this->_options);
 	}
-	
+
 	/**
 	 * Gives a combiner object for CSS/JS
 	 *
@@ -43,19 +49,19 @@ abstract class JMediaCombiner
 	 */
 	public static function getInstance($type, $options = array())
 	{
-	
+
 		// Derive the class name from the type.
 		$class = 'JMediaCombiner' . ucfirst(strtolower($type));
-	
+
 		// Load the class
 		jimport('joomla.media.combiner.' . $class);
-	
+
 		// If the class still doesn't exist we have nothing left to do but throw an exception.  We did our best.
 		if (!class_exists($class))
 		{
 			throw new RuntimeException(JText::sprintf('JMEDIA_ERROR_LOAD_COMBINOR', $type));
 		}
-	
+
 		// Create our new JMediaCompressor class based on the options given.
 		try
 		{
@@ -65,14 +71,21 @@ abstract class JMediaCombiner
 		{
 			throw new RuntimeException(JText::sprintf('JLIB_DATABASE_ERROR_CONNECT_DATABASE', $e->getMessage()));
 		}
-	
+
 		return $instance;
 	}
-	
+
+	/**
+	 * 
+	 * @param unknown_type $files
+	 * @param unknown_type $options
+	 * @param unknown_type $destination
+	 * @throws RuntimeException
+	 */
 	public static function combineFiles($files, $options = array(), $destination = null)
 	{
 		$type = JFile::getExt($files[0]);
-		
+
 		foreach ($files as $file)
 		{
 			if ($type != JFile::getExt($file))
@@ -80,9 +93,9 @@ abstract class JMediaCombiner
 				throw new RuntimeException(JText::sprintf('JMEDIA_COMBINE_ERROR_MULTIPLE_FILE_TYPES'), $type);
 			}
 		}
-		
+
 		$combiner = self::getInstance($type);
-		
+
 		$combiner->combine($files);
 	}
 }
