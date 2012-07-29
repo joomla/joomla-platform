343dc6c5e0dab248a9af9cac850eb5fdc13f060d
kaviththiranga <rc404mekt@gmail.com>
2012-07-29 09:40:45 +0530

    Test files implemented

---
+++ tests/suites/unit/joomla/media/JMediaCombinerTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package     Joomla.UnitTest
+ * @subpackage  Media
+ *
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+jimport('joomla.filesystem.file');
+jimport('joomla.filesystem.folder');
+
+/**
+ * Test class for JMediaCompressor.
+ */
+class JMediaCombinerTest extends TestCase
+{
+    /**
+     * @var JMediaCompressor
+     */
+    protected $object;
+
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = JMediaCombiner::getInstance(array('type' => 'css'));
+    }
+
+   
+}
