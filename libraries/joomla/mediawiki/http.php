1221e529db4d51e2c5d2388d28cffb2eec39dd8b
Prasath Nadarajah <n.prasath.002@gmail.com>
2012-08-12 12:00:15 +0530

    removing user-agent

--- libraries/joomla/mediawiki/http.php
+++ libraries/joomla/mediawiki/http.php
@@ -31,11 +31,5 @@ class JMediawikiHttp extends JHttp
 		// Override the JHttp contructor to use JHttpTransportStream.
 		$this->options = isset($options) ? $options : new JRegistry;
 		$this->transport = isset($transport) ? $transport : new JHttpTransportStream($this->options);
-
-		// Make sure the user agent string is defined.
-		$this->options->def('userAgent', 'JMediawiki/1.0');
-
-		// Set the default timeout to 120 seconds.
-		$this->options->def('timeout', 120);
 	}
 }
