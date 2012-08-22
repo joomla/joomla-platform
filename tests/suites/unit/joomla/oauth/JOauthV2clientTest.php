936133c114a03d566783beb209516efd130142f3
Aaron Schmitz <me@aaronschmitz.com>
2012-08-22 02:12:23 -0500

    Rename auth -> authenticate Rename isAuth -> isAuthenticated Replace bool -> boolean

--- tests/suites/unit/joomla/oauth/JOauthV2clientTest.php
+++ tests/suites/unit/joomla/oauth/JOauthV2clientTest.php
@@ -80,7 +80,7 @@ class JOauthV2clientTest extends PHPUnit_Framework_TestCase
 		$this->object->setOption('requestparams', array('access_type' => 'offline', 'approval_prompt' => 'auto'));
 		$this->object->setOption('sendheaders', true);
 
-		$this->object->auth();
+		$this->object->authenticate();
 		$headers = JResponse::getHeaders();
 		$location = false;
 		foreach ($headers as $header)
@@ -98,7 +98,7 @@ class JOauthV2clientTest extends PHPUnit_Framework_TestCase
 		$this->input->set('code', '4/wEr_dK8SDkjfpwmc98KejfiwJP-f4wm.kdowmnr82jvmeisjw94mKFIJE48mcEM');
 
 		$this->http->expects($this->once())->method('post')->will($this->returnCallback('encodedGrantOauthCallback'));
-		$result = $this->object->auth();
+		$result = $this->object->authenticate();
 		$this->assertEquals('accessvalue', $result['access_token']);
 		$this->assertEquals('refreshvalue', $result['refresh_token']);
 		$this->assertEquals(3600, $result['expires_in']);
@@ -118,7 +118,7 @@ class JOauthV2clientTest extends PHPUnit_Framework_TestCase
 		$this->input->set('code', '4/wEr_dK8SDkjfpwmc98KejfiwJP-f4wm.kdowmnr82jvmeisjw94mKFIJE48mcEM');
 
 		$this->http->expects($this->once())->method('post')->will($this->returnCallback('jsonGrantOauthCallback'));
-		$result = $this->object->auth();
+		$result = $this->object->authenticate();
 		$this->assertEquals('accessvalue', $result['access_token']);
 		$this->assertEquals('refreshvalue', $result['refresh_token']);
 		$this->assertEquals(3600, $result['expires_in']);
@@ -133,7 +133,7 @@ class JOauthV2clientTest extends PHPUnit_Framework_TestCase
 	 */
 	public function testIsAuth()
 	{
-		$this->assertEquals(false, $this->object->isAuth());
+		$this->assertEquals(false, $this->object->isAuthenticated());
 
 		$token['access_token'] = 'accessvalue';
 		$token['refresh_token'] = 'refreshvalue';
@@ -141,13 +141,13 @@ class JOauthV2clientTest extends PHPUnit_Framework_TestCase
 		$token['expires_in'] = 3600;
 		$this->object->setToken($token);
 
-		$this->assertTrue($this->object->isAuth());
+		$this->assertTrue($this->object->isAuthenticated());
 
 		$token['created'] = time() - 4000;
 		$token['expires_in'] = 3600;
 		$this->object->setToken($token);
 
-		$this->assertFalse($this->object->isAuth());
+		$this->assertFalse($this->object->isAuthenticated());
 	}
 
 	/**
