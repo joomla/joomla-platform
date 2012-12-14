ffff4d8f8bb68d05e57f3757836c6739d6b65975
Kavith Thiranga <rc404mekt@gmail.com>
2012-12-14 11:50:23 +0530

    removed conflicts in JRequestGetMethod.php

--- tests/suites/legacy/request/JRequestGetMethodTest.php
+++ tests/suites/legacy/request/JRequestGetMethodTest.php
@@ -2,7 +2,6 @@
 /**
  * @package     Joomla.UnitTest
  * @subpackage  Request
-
  *
  * @copyright   Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
